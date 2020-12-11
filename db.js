c06d71a72422900125538abf99a29ab7d78f239b
RossTheDev <techbyross@gmail.com>
2020-12-11 14:07:57 -0600

    added date/time plus some more css for the dir

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
-// Use this field to fill out user information and tasks
-var name = "P. Ennis"
+//Company details
+var companyname = "Example Company"
+
+//Employee details
+var name = "Name"
+
+//Tasks
 var tasks = "<p> -Finish the contact page</p>";
     tasks += "<p> -Test the waters </p>";
     // ^ Multiple tasks example. To update just rewrite whats in the "" or to add a new task CTRL C+ CTRL R to redo another line then update the "".
@@ -7,3 +12,9 @@ var tasks = "<p> -Finish the contact page</p>";
 function docWrite(variable) {
     document.write(variable);
 }
+
+//displays date/time/timzone
+var dt = new Date();
+
+//prevents right click
+document.addEventListener('contextmenu', event => event.preventDefault());
