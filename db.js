9e122d5f51e3340a136a2dcce5a64b01f7c45f98
RossTheDev <techbyross@gmail.com>
2020-12-12 12:09:23 -0600

    redid some css, redid index setup, and more.

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 //Company details
 var companyname = "Example Company"
+var phonen = "(555)-505-5555"
 
 //Employee details
 var name = "Name"
 
 //Tasks
 var tasks = "<br>-Finish the contact page";
-    tasks += "<br>-Test the waters ";
+    tasks += "<br>-Work on CSS ";
+    tasks += "<br>-Make Wiki  ";
+    tasks += "<br>-Set up Netlify";
+    tasks += "<br>-Setup Electron ";
+
     // ^ Multiple tasks example. To update just rewrite whats in the "" or to add a new task CTRL C+ CTRL R to redo another line then update the "".
