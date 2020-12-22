9ee06b9ec59062bc6211305df1a17ae13843a161
RossTheDev <ross.f.pittser@my.occc.edu>
2020-12-21 18:39:27 -0600

    index progress and overall touchup

--- scripts.js
+++ scripts.js
@@ -6,5 +6,12 @@ function Write(variable) {
 //displays date/time/timzone
 var dt = new Date();
 
+//features
+var features = "-Unlimited user accounts.";
+      features += "<br>-Company wide bulletins. ";
+      features += "<br>-Free hosting with netlify. ";
+      features += "<br>-Easy to use.";
+      features += "<br>-Use it anywhere.";
+
 //prevents right click
 document.addEventListener('contextmenu', event => event.preventDefault());
