08df6553ed3a8e4e8ad7b9fe07fb65ddb68d8ee5
RossTheDev <techbyross@gmail.com>
2021-01-12 10:43:06 -0600

    Added lists + example

---
+++ ready2use/MultipleTasksEx/tasks.js
@@ -0,0 +1,14 @@
+//Tasks
+var jdtasks = "<br>-Spruce up the look";
+    jdtasks += "<br>-Work on CSS ";
+    jdtasks += "<br>-Make Wiki  ";
+    jdtasks += "<br>-Set up Netlify";
+    jdtasks += "<br>-Setup Electron ";
+
+    // ^ Multiple tasks example. To update just rewrite whats in the "" past the <br> or to add a new task CTRL C+ CTRL R to redo another line then update the "".
+  var tjtasks = "<br>-Send email to corporate";
+        tjtasks += "<br>-Work on faxs ";
+        tjtasks += "<br>-Meeting @ 3  ";
+        tjtasks += "<br>-Check on development";
+        tjtasks += "<br>-Ship Product";
+//^ mutiple people example just call this  file for every person's page.
